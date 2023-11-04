fe889eaf5ff90f802ce29dbc4c66bf6eb894f1dd
MichelleLin5 <ml7188@nyu.edu>
2023-11-04 12:43:31 -0400

    updated state initialization in main

--- Puzzles.py
+++ Puzzles.py
@@ -24,13 +24,19 @@ def find_tile_position(state, tile):
                 if state[x][y][z] == tile:
                     return x, y, z
 
+def find_reachable_states(state):
+    # Possible actions: East (E), West (W), North (N), South (S), Up (U) and Down (D)
+    pass
+
+
 # Define the A* search node class
 class Node:
-    def __init__(self, state: List[List[List[int]]], depth: int, cost: int, parent=None):
+    def __init__(self, state: List[List[List[int]]], depth: int, cost: int, parent=None, action=None):
         self.state = state
         self.depth = depth
         self.cost = cost
         self.parent = parent
+        self.action = action
 
     def __lt__(self, other):
         return self.cost < other.cost
@@ -43,7 +49,24 @@ def astar_search(initial_state, goal_state):
     # initial_node = Node(initial_state, 0, 0)
     # heapq.heappush(open_set, initial_node)
     pass
+    # while len(open_set) > 0:
+    #     current_node = open_set[0] #pop the node with least cost to explore
 
+    #DECLARE A NODE: Node(state, depth, cost, parent)
+
+    #find all of the reachable nodes from initial state
+
+    #add them to the heap
+
+    #declare a reached state heap
+
+    #while the heap is not empty
+        #find the node with the least f(n), call it current_node
+        #pop current_node off the heap
+        #if curren_node == goal_state, stop search and return
+        #generate curren_node's successors and have their parents as current_node, save it in the heap
+        #no repeated states allowed - so if successor in reached state OR in heap, don't generate
+        
 # Main function
 def main() -> None:
     # Parse command-line arguments
@@ -55,11 +78,25 @@ def main() -> None:
     with open(cmdline.filename, 'r') as file:
         lines = file.read().splitlines()
 
-    initial_state = [list(map(int, line.split())) for line in lines[:11]]  # Initial state
-    goal_state = [list(map(int, line.split())) for line in lines[12:24]]  # Goal state
+    # initial_state = [list(map(int, line.split())) for line in lines[:11]]  # Initial state
+    # goal_state = [list(map(int, line.split())) for line in lines[12:24]]  # Goal state
+
+    initial_state = [[list(map(int, line.split())) for line in lines[:3]]]  # Initial state
+    initial_state += [[list(map(int, line.split())) for line in lines[4:7]]]
+    initial_state += [[list(map(int, line.split())) for line in lines[8:11]]]
+
+    goal_state = [[list(map(int, line.split())) for line in lines[12:15]]]
+    goal_state += [[list(map(int, line.split())) for line in lines[16:19]]]
+    goal_state += [[list(map(int, line.split())) for line in lines[20:23]]]
 
     # Find the solution using A* search
-    
+    print("intital state:")
+    print(initial_state)
+    print("goal state")
+    print(goal_state)
+
+    # print(find_tile_position(initial_state, 2))
+    # print(manhattan_distance([initial_state], [goal_state]))
 
 if __name__ == "__main__":
     main()
