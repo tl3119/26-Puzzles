91de09445750250fdd2b1b57ce2cfffbdb2768db
MichelleLin5 <ml7188@nyu.edu>
2023-11-05 15:55:44 -0500

    output generated

--- Puzzles.py
+++ Puzzles.py
@@ -31,28 +31,28 @@ def find_reachable_states(state):
     reachable_states_actions = []
     # East:
     if find_tile_position(state, 0)[2] != 2: # not in the third column of any level of tiles
-        reachable_states += [get_state(state, "East")]
-        reachable_states_actions += ["East"]
+        reachable_states += [get_state(state, "E")]
+        reachable_states_actions += ["E"]
     # West:
     if find_tile_position(state, 0)[2] != 0: # not in the first column of any level of tiles
-        reachable_states += [get_state(state, "West")]
-        reachable_states_actions += ["West"]
+        reachable_states += [get_state(state, "W")]
+        reachable_states_actions += ["W"]
     # North:
     if find_tile_position(state, 0)[1] != 0: # not in the first row of any level of tiles
-        reachable_states += [get_state(state, "North")]
-        reachable_states_actions += ["North"]
+        reachable_states += [get_state(state, "N")]
+        reachable_states_actions += ["N"]
     # South:
     if find_tile_position(state, 0)[1] != 2: # not in the third row of any level of tiles
-        reachable_states += [get_state(state, "South")]
-        reachable_states_actions += ["South"]
+        reachable_states += [get_state(state, "S")]
+        reachable_states_actions += ["S"]
     # Up:
     if find_tile_position(state, 0)[0] != 0: # not in the first level of tiles
-        reachable_states += [get_state(state, "Up")]
-        reachable_states_actions += ["Up"]
+        reachable_states += [get_state(state, "U")]
+        reachable_states_actions += ["U"]
     # Down:
     if find_tile_position(state, 0)[0] != 2: # not in the third level of tiles
-        reachable_states += [get_state(state, "Down")]
-        reachable_states_actions += ["Down"]
+        reachable_states += [get_state(state, "D")]
+        reachable_states_actions += ["D"]
 
     return reachable_states, reachable_states_actions
 
@@ -61,33 +61,33 @@ def get_state(state, action):
     # print(state)
     tile_position = find_tile_position(state, 0)
     new_state = copy.deepcopy(state)
-    if action == "East": # go to the right, possible sample input: ([0, 1, 2] or [1, 0, 2])
+    if action == "E": # go to the right, possible sample input: ([0, 1, 2] or [1, 0, 2])
         if tile_position[2] == 0: # [0, 1, 2]
             # new_state = state
             new_state[tile_position[0]][tile_position[1]] = [state[tile_position[0]][tile_position[1]][1], 0, state[tile_position[0]][tile_position[1]][2]]
         elif tile_position[2] == 1: # [1, 0, 2]
             # new_state = state
             new_state[tile_position[0]][tile_position[1]] = [state[tile_position[0]][tile_position[1]][0], state[tile_position[0]][tile_position[1]][2], 0]
-    elif action == "West": # go to the left, possible sample input: ([1, 0, 2] or [1, 2, 0])
+    elif action == "W": # go to the left, possible sample input: ([1, 0, 2] or [1, 2, 0])
         if tile_position[2] == 1: # [1, 0, 2]
             # new_state = state
             new_state[tile_position[0]][tile_position[1]] = [0, state[tile_position[0]][tile_position[1]][0], state[tile_position[0]][tile_position[1]][2]]
         elif tile_position[2] == 2: # [1, 2, 0]
             # new_state = state
             new_state[tile_position[0]][tile_position[1]] = [state[tile_position[0]][tile_position[1]][0], 0, state[tile_position[0]][tile_position[1]][1]]
-    elif action == "South": # slide up on the same level
+    elif action == "S": # slide up on the same level
         # new_state = state
         new_state[tile_position[0]][tile_position[1]][tile_position[2]] = new_state[tile_position[0]][tile_position[1]+1][tile_position[2]]
         new_state[tile_position[0]][tile_position[1]+1][tile_position[2]] = 0
-    elif action == "North": # slide down on the same level
+    elif action == "N": # slide down on the same level
         # new_state = state
         new_state[tile_position[0]][tile_position[1]][tile_position[2]] = new_state[tile_position[0]][tile_position[1]-1][tile_position[2]]
         new_state[tile_position[0]][tile_position[1]-1][tile_position[2]] = 0
-    elif action == "Up": # go up a level, possible input: on level 1 or 2
+    elif action == "U": # go up a level, possible input: on level 1 or 2
         # new_state = state
         new_state[tile_position[0]][tile_position[1]][tile_position[2]] = state[tile_position[0]-1][tile_position[1]][tile_position[2]]
         new_state[tile_position[0]-1][tile_position[1]][tile_position[2]] = 0
-    elif action == "Down": # go down a level, possible input: on level 0 or 1
+    elif action == "D": # go down a level, possible input: on level 0 or 1
         # new_state = state
         new_state[tile_position[0]][tile_position[1]][tile_position[2]] = state[tile_position[0]+1][tile_position[1]][tile_position[2]]
         new_state[tile_position[0]+1][tile_position[1]][tile_position[2]] = 0
@@ -115,6 +115,8 @@ def astar_search(initial_state, goal_state):
     frontier = [initial_node] # current frontier
     reached = [initial_node.state] # reached states
 
+    nodes_generated_count = 0
+
     while len(frontier) > 0:
     # for i in range(2):
         #get node in frontier with least cost
@@ -131,7 +133,9 @@ def astar_search(initial_state, goal_state):
         # print(current_node.state)
         if current_node.state == goal_state: # check goal state before expansion
             print("solution found!!!")
-            return current_node.state
+            generate_output_file(current_node, nodes_generated_count)
+            return None
+            # return current_node.state
         reachable_states, reachable_states_actions = find_reachable_states(current_node.state) # expand
         # print("reachable state actions: ", reachable_states_actions)
         # for i in range(len(reachable_states)):
@@ -140,7 +144,10 @@ def astar_search(initial_state, goal_state):
             
         for i in range(len(reachable_states)):
             if reachable_states[i] not in reached:
-                new_node = Node(reachable_states[i], 1, manhattan_distance(reachable_states[i], goal_state), parent = current_node, action = reachable_states_actions[i])
+                new_node = Node(reachable_states[i], current_node.depth+1, manhattan_distance(reachable_states[i], goal_state), parent = current_node, action = reachable_states_actions[i])
+                nodes_generated_count += 1
+                # print("current node depth")
+                # print(new_node.depth)
                 frontier.append(new_node)
                 reached.append(new_node.state)
                 # print("appended!")
@@ -154,6 +161,56 @@ def is_goal(state, goal_state):
         return True
     else:
         return False
+    
+def generate_output_file(node, nodes_generated_count):
+    # copy paste the initial state and goal state to output file
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    cmdline = parser.parse_args()
+
+    # Read input file and create initial and goal states
+    with open(cmdline.filename, 'r') as file:
+        lines = file.read().splitlines()
+    
+    new_file_name = cmdline.filename[:-4] + "solution.txt"
+    f = open(new_file_name, "w")
+    for i in lines:
+        f.write(i)
+        f.write("\n")
+
+    # Line 24 is a blank line
+    f.write ("\n")
+
+    # Line 25 is the depth level d of the shallowest goal node as found by the A* algorithm 
+    # (assume the root node is at level 0.) 
+    f.write(str(node.depth))
+    f.write ("\n")    
+    # Line 26 is the total number of nodes N generated in your tree (including the root node.) 
+    f.write(str(nodes_generated_count))
+    f.write ("\n")    
+    # Line 27 contains the solution (a sequence of actions from root node to goal node) represented by A’s.
+    solution = []
+    solution_path_cost = []
+    while node.parent != None: # trace back the solution from leaf node
+        solution.append(node.action)
+        solution_path_cost.append(node.cost + node.depth) # f(n)
+        # print("appended node depth")
+        # print(node.depth)
+        # print("appended node cost")
+        # print(node.cost)
+        node = node.parent
+    solution_path_cost.append(node.cost + node.depth) # d+1, parent node f(n)
+    solution.reverse() # make the list solution in reverse (so that it comes from root node)
+    for i in range(len(solution)):
+        f.write(solution[i])
+    f.write ("\n")    
+    # Line 28 contains the f(n) values of the nodes along the solution path, 
+    # from the root node to the goal node, separated by blank spaces. 
+    solution_path_cost.reverse()
+    for i in range(len(solution_path_cost)):
+        f.write(str(solution_path_cost[i]))
+        f.write(" ")
+    f.write ("\n")  
 
 # Main function
 def main() -> None:
@@ -178,11 +235,11 @@ def main() -> None:
     goal_state += [[list(map(int, line.split())) for line in lines[20:23]]]
 
     # Find the solution using A* search
-    print(astar_search(initial_state, goal_state))
-    print("intital state:")
-    print(initial_state)
-    print("goal state")
-    print(goal_state)
+    astar_search(initial_state, goal_state)
+    # print("intital state:")
+    # print(initial_state)
+    # print("goal state")
+    # print(goal_state)
     # print("find tile position")
     # print(find_tile_position(initial_state, 22))
     # print("find manhattan")
